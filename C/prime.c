2543527d059038833f8825c30d89604c014668be
Adil Husain <adil.husain@lpu.in>
2023-05-07 12:00:11 +0530

    07.Apr.2023 Prime numbers without using functions

--- C/prime.c
+++ C/prime.c
@@ -1,14 +1,22 @@
+//Finding Prime numbers in a range without using functions
+
 #include <stdio.h>
 
 int main(){
-    int a;
-    printf("Enter the value : ");
-    scanf("%d",&a);
-    printf("%d",a);
-    printf("24");
-}
-void okay(){
-    printf("Hello");
-    printf("ugly");
-    printf("asfuck");
+    int x=0;
+    printf("Enter a number: ");
+    scanf("%d",&x);
+    for(int i=2;i<x;i++){
+        int flag=0;
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0){
+            printf("%d ",i);
+        }
+    }
+
 }
