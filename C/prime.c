278c2eb26fca0f02fe658dbe465163334e9911f3
Adil Husain <adil.husain@lpu.in>
2023-05-07 15:37:15 +0530

    Prime numbers in python without using functions

--- C/prime.c
+++ C/prime.c
@@ -1,21 +1,25 @@
-//Finding Prime numbers in a range without using functions
-
+//Finding Prime numbers in a range without using functions 07.05.2023
 #include <stdio.h>
-
 int main(){
-    int x=0;
+    int x=0,flag=0;
     printf("Enter a number: ");
     scanf("%d",&x);
-    for(int i=2;i<x;i++){
-        int flag=0;
-        for(int j=2;j<i;j++){
-            if(i%j==0){
-                flag=1;
-                break;
+    if(x<=1){
+        printf("Its is not a prime number");
+    }else{  
+        for(int i=2;i<x;i++){
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    flag=1;
+                    break;
+                }
             }
         }
         if(flag==0){
-            printf("%d ",i);
+            printf("It is a prime number");
+        } else if (flag==1){
+            printf("It is not a prime number");
         }
     }
+    return 0;
 }
--- PYTHON/prime.py
+++ PYTHON/prime.py
@@ -1,2 +1,14 @@
-for i in range(10):
-    print(i)
+#prime numbers without using functions
+flag=0
+x=int(input("Enter a number: "))
+if (x<=1):
+    print("Not Prime")
+else:
+    for i in range(2,x):
+        if x%i==0:
+            flag=1
+            break
+    if flag==0:
+        print("Prime")
+    else:
+        print("Not Prime")
